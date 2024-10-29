97a326b17389a38e366a1d7c5893f0eb401343c1
Erkki Sinkko <erkki.sinkko@hyria.fi>
2024-10-29 10:31:38 +0200

    vaihe 3

--- budjetti.py
+++ budjetti.py
@@ -5,6 +5,10 @@
 # Nämä käyttäjän lisäämät budjettikohteet tallennetaan samaan rakenteeseen, johon myös ennakkoon määritellyt kohteet on tallennettu.
 # monirivinen kursori oli: Ctrl + Alt + nuoli alaspäin
 
+# Vaihe 3: Budjettikohteiden muokkaaminen
+# Laajenna ohjelmaa siten, että käyttäjä voi muokata aiemmin syöttämiään budjettikohteita.
+# Anna käyttäjälle mahdollisuus valita, mitä budjettikohdetta hän haluaa muokata, ja syöttää uusi summa.
+
 budjetti = {
     "Vuokra": 0, 
     "Vakuutukset": 0,
@@ -19,17 +23,49 @@ print("---------------------")
 for key, value in budjetti.items():
     budjetti[key] = int(input(f"Anna {key}: "))
 
-print("-------")
-print("Syötä 'q', jos haluat lopettaa kulujen syöttämisen.")
-while True: 
+# kokonaisbudjetin laskenta ja tulostus
+def tulostaBudjetti(budjetti): 
+    
+    print("---------")
+    print("Kuluerät: ")
+    summa = 0
+    for key, value in budjetti.items():
+        print(f"{key}: {value}")
+        summa += budjetti[key]
+    print("==================================")
+    print(f"Kuukauden budjettisi on:  {summa}")
+
+def lisaaKulu(budjetti): 
     syote = input("Anna uuden kuluerän nimi: ")
-    if syote == 'q':
-        break
     budjetti[syote] = int(input(f"Anna {syote}: "))
 
-# kokonaisbudjetin laskenta
-summa = 0
-for key, value in budjetti.items():
-    summa += budjetti[key]
+def muokkaa(budjetti): 
+    for key, value in budjetti.items(): 
+        print(key) 
+    syote = input("Mitä haluat muokata?")
+    budjetti[syote] = int(input(f"Anna {syote}: "))
 
-print(f"Kuukauden budjettisi on:  {summa}")
+
+print("-------")
+while True: 
+    print("Komennot:")
+    print("(l)isää kulu")
+    print("(m)uokkaa kulua")
+    print("(t)ulosta budjetti")
+    print("(q) poistu ohjelmasta")
+    syote = input("Komento: ")
+    match syote: 
+        case 'l': 
+            lisaaKulu(budjetti)
+        case 'm': 
+            muokkaa(budjetti)
+        case 't':
+            tulostaBudjetti(budjetti)
+        case 'q': 
+            break
+        case __:
+            print('Tuntematon komento')
+            break
+
+
+    
