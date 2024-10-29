1e40c48c74ea869d21c03819a90e04d569f65e20
Erkki Sinkko <erkki.sinkko@hyria.fi>
2024-10-29 08:01:11 +0200

    vaihe 1

--- budjetti.py
+++ budjetti.py
@@ -1,1 +1,28 @@
-#Tähän pitäisi tulla koodia: ...
+# Tähän pitäisi tulla koodia: ...
+
+# Vuokra
+# Vakuutukset
+# Kouluruokailu
+# Sähkö
+# Liikkuminen
+# Voit tallentaa nämä tiedot esimerkiksi sanakirjaan (dictionary), jossa avaimet ovat kululajien nimiä ja arvot ovat kulujen summia.
+
+budjetti = {
+    "Vuokra": 0, 
+    "Vakuutukset": 0,
+    "Kouluruokailu": 0, 
+    "Sähkö": 0, 
+    "Liikkuminen": 0
+}
+
+print("Hei, anna budjetti-kohteet ja lasken kuukauden kokonaisbudjetin!")
+print("---------------------")
+
+for key, value in budjetti.items():
+    budjetti[key] = int(input(f"Anna {key}: "))
+
+summa = 0
+for key, value in budjetti.items(): 
+    summa += value
+
+print(f"Kuukauden budjettisi on:  {summa}")
