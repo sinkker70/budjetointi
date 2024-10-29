8ac779b289397108232e682683ebb2a716bce94b
Erkki Sinkko <erkki.sinkko@hyria.fi>
2024-10-29 08:26:12 +0200

    vaihe 2

--- budjetti.py
+++ budjetti.py
@@ -1,11 +1,9 @@
 # Tähän pitäisi tulla koodia: ...
-
-# Vuokra
-# Vakuutukset
-# Kouluruokailu
-# Sähkö
-# Liikkuminen
-# Voit tallentaa nämä tiedot esimerkiksi sanakirjaan (dictionary), jossa avaimet ovat kululajien nimiä ja arvot ovat kulujen summia.
+# Vaihe 2: Mukautettavien budjettikohteiden lisääminen
+# Laajenna ohjelmaa siten, että käyttäjä voi lisätä omia budjettikohteita.
+# Ohjelman tulee pyytää käyttäjää antamaan uuden kuluerän nimi ja siihen budjetoitu summa.
+# Nämä käyttäjän lisäämät budjettikohteet tallennetaan samaan rakenteeseen, johon myös ennakkoon määritellyt kohteet on tallennettu.
+# monirivinen kursori oli: Ctrl + Alt + nuoli alaspäin
 
 budjetti = {
     "Vuokra": 0, 
@@ -21,8 +19,17 @@ print("---------------------")
 for key, value in budjetti.items():
     budjetti[key] = int(input(f"Anna {key}: "))
 
+print("-------")
+print("Syötä 'q', jos haluat lopettaa kulujen syöttämisen.")
+while True: 
+    syote = input("Anna uuden kuluerän nimi: ")
+    if syote == 'q':
+        break
+    budjetti[syote] = int(input(f"Anna {syote}: "))
+
+# kokonaisbudjetin laskenta
 summa = 0
-for key, value in budjetti.items(): 
-    summa += value
+for key, value in budjetti.items():
+    summa += budjetti[key]
 
 print(f"Kuukauden budjettisi on:  {summa}")
